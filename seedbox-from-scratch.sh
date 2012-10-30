2d52c3e3ab462f2303cd56d73e78843f0146c8fb
Notos <antoniocarlos.demonoid@cys.com.br>
2012-10-30 12:37:29 -0200

    directories problem
    Signed-off-by: Notos <antoniocarlos.demonoid@cys.com.br>

--- seedbox-from-scratch.sh
+++ seedbox-from-scratch.sh
@@ -352,7 +352,7 @@ tar xvfz /etc/seedbox-from-scratch/rtorrent-0.9.2.tar.gz -C /etc/seedbox-from-sc
 tar xvfz /etc/seedbox-from-scratch/libtorrent-0.12.9.tar.gz -C /etc/seedbox-from-scratch/source/
 tar xvfz /etc/seedbox-from-scratch/libtorrent-0.13.2.tar.gz -C /etc/seedbox-from-scratch/source/
 tar xvfz /etc/seedbox-from-scratch/xmlrpc-c-1.16.42.tgz -C /etc/seedbox-from-scratch/source/
-cd source
+cd /etc/seedbox-from-scratch/source/
 unzip ../xmlrpc-c-1.31.06.zip
 
 # 16.
