c5c4e74190a23f31a513f0240c6dbe54e0b41780
Notos <notos.korsan@gmail.com>
2012-12-01 17:31:27 -0200

    first run on Debian 6

--- seedbox-from-scratch.sh
+++ seedbox-from-scratch.sh
@@ -231,14 +231,18 @@ else
   LIBTORRENT1=0.12.9
 fi
 
-apt-get --yes install whois sudo makepasswd git python-software-properties software-properties-common
+apt-get --yes install whois sudo makepasswd git
 
 rm -f -r /etc/seedbox-from-scratch
 git clone -b v$SBFSCURRENTVERSION https://github.com/Notos/seedbox-from-scratch.git /etc/seedbox-from-scratch
 mkdir -p cd /etc/seedbox-from-scratch/source
 mkdir -p cd /etc/seedbox-from-scratch/users
 
-sudo add-apt-repository --yes ppa:thefrontiergroup/vsftpd
+if [ ! -f /etc/debian_version ]
+  apt-get --yes install python-software-properties
+  apt-get --yes install software-properties-common
+  sudo add-apt-repository --yes ppa:thefrontiergroup/vsftpd
+fi
 
 if [ ! -f /etc/seedbox-from-scratch/seedbox-from-scratch.sh ]
 then
@@ -272,21 +276,21 @@ service ssh restart
 #remove cdrom from apt so it doesn't stop asking for it
 perl -pi -e "s/deb cdrom/#deb cdrom/g" /etc/apt/sources.list
 
-#add non-free sources to Debian Squeeze
-perl -pi -e "s/squeeze main/squeeze main non-free/g" /etc/apt/sources.list
-perl -pi -e "s/squeeze\/updates main/squeeze\/updates main non-free/g" /etc/apt/sources.list
+#add non-free sources to Debian Squeeze# those two spaces below are on purpose
+perl -pi -e "s/squeeze main/squeeze  main contrib non-free/g" /etc/apt/sources.list
+perl -pi -e "s/squeeze-updates main/squeeze-updates  main contrib non-free/g" /etc/apt/sources.list
 
 # 7.
 # update and upgrade packages
 
-sudo apt-get --yes update
+apt-get --yes update
 apt-get --yes upgrade
 
 # 8.
 #install all needed packages
 
 apt-get --yes build-dep znc
-apt-get --yes install apache2 apache2-utils autoconf build-essential ca-certificates comerr-dev curl cfv quota mktorrent dtach htop irssi libapache2-mod-php5 libcloog-ppl-dev libcppunit-dev libcurl3 libcurl4-openssl-dev libncurses5-dev libterm-readline-gnu-perl libsigc++-2.0-dev libperl-dev openvpn libssl-dev libtool libxml2-dev ncurses-base ncurses-term ntp openssl patch libc-ares-dev pkg-config php5 php5-cli php5-dev php5-curl php5-geoip php5-mcrypt php5-gd php5-xmlrpc pkg-config python-scgi screen ssl-cert subversion texinfo unrar-free unzip zlib1g-dev expect joe automake1.9 flex bison debhelper binutils-gold ffmpeg libarchive-zip-perl libnet-ssleay-perl libhtml-parser-perl libxml-libxml-perl libjson-perl libjson-xs-perl libxml-libxslt-perl libxml-libxml-perl libjson-rpc-perl libarchive-zip-perl rar zip python-software-properties vsftpd znc
+apt-get --yes install apache2 apache2-utils autoconf build-essential ca-certificates comerr-dev curl cfv quota mktorrent dtach htop irssi libapache2-mod-php5 libcloog-ppl-dev libcppunit-dev libcurl3 libcurl4-openssl-dev libncurses5-dev libterm-readline-gnu-perl libsigc++-2.0-dev libperl-dev openvpn libssl-dev libtool libxml2-dev ncurses-base ncurses-term ntp openssl patch libc-ares-dev pkg-config php5 php5-cli php5-dev php5-curl php5-geoip php5-mcrypt php5-gd php5-xmlrpc pkg-config python-scgi screen ssl-cert subversion texinfo unzip zlib1g-dev expect joe automake1.9 flex bison debhelper binutils-gold ffmpeg libarchive-zip-perl libnet-ssleay-perl libhtml-parser-perl libxml-libxml-perl libjson-perl libjson-xs-perl libxml-libxslt-perl libxml-libxml-perl libjson-rpc-perl libarchive-zip-perl python-software-properties vsftpd znc
 if [ $? -gt 0 ]; then
   set +x verbose
   echo
@@ -300,6 +304,19 @@ if [ $? -gt 0 ]; then
   set -e
   exit 1
 fi
+apt-get --yes install zip
+
+apt-get --yes install rar
+if [ $? -gt 0 ]; then
+  apt-get --yes install rar-free
+fi
+
+apt-get --yes install unrar
+if [ $? -gt 0 ]; then
+  apt-get --yes install unrar-free
+fi
+
+apt-get --yes install dnsutils
 
 if [ "$CHROOTJAIL1" = "YES" ]; then
   cd /etc/seedbox-from-scratch
